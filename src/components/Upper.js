170b54d650be5cb6e29551ebb49bdeafa6fd9cc9
k_krylowicz <kacper@krylowicz.pl>
2019-12-20 21:45:26 +0100

    finished basic Upper component layout

--- src/components/Upper.js
+++ src/components/Upper.js
@@ -9,6 +9,11 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 const Heading = styled.h1`
   font-weight: 500;
   letter-spacing: 1.2px;
@@ -16,13 +21,47 @@ const Heading = styled.h1`
 `;
 
 const Input = styled.input`
+  font-size: 1.5rem;
+  border: none;
+  margin-bottom: 2px;
+  text-align: center;
+  
+  &:focus {
+    outline: none;
+  }
+  
+  &::placeholder {
+    text-align: center;
+  }
+`;
 
+const ItemBar = styled.div`
+  width: 100%;
+  height: 2px;
+  background: #7d7d7d;
+  border-radius: 25px;
+`;
+
+const Button = styled.button`
+  margin-left: 25px;
+  border: none;
+  padding: 7px 15px;
+  background-color: #1976D2;
+  color: white;
+  border-radius: 25px;
+  font-size: 1.2rem;
 `;
 
 const Upper = () => (
     <Wrapper>
         <Heading>to-do app</Heading>
-        <Input placeholder="add to-do task" />
+        <Row>
+            <div>
+                <Input placeholder="add task" />
+                <ItemBar />
+            </div>
+            <Button>add</Button>
+        </Row>
     </Wrapper>
 );
 
