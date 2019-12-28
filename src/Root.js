ca53f1286a6290f9801dd4f46b44fb4f910d99fb
k_krylowicz <kacper@krylowicz.pl>
2019-12-28 15:27:45 +0100

    refactored application

--- src/Root.js
+++ src/Root.js
@@ -1,37 +1,93 @@
-import React from 'react';
+import React, { useState } from "react";
 import './index.css';
-import Upper from './components/Upper/Upper';
-import Lower from "./components/Lower/Lower";
 
-class Root extends React.Component {
-    state = {
-        userInput: "",
-        list: []
+function Todo({ todo, index, completeTodo, removeTodo }) {
+    return (
+        <div
+            className="todo"
+            style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+        >
+            {todo.text}
+
+            <div>
+                <button onClick={() => completeTodo(index)}>Complete</button>
+                <button onClick={() => removeTodo(index)}>x</button>
+            </div>
+        </div>
+    );
+}
+
+function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue("");
     };
 
-    handleChange = e => {
-        this.setState({userInput: e});
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+        </form>
+    );
+}
+
+function Root() {
+    const [todos, setTodos] = useState([
+        {
+            text: "Learn about React",
+            isCompleted: false
+        },
+        {
+            text: "Meet friend for lunch",
+            isCompleted: false
+        },
+        {
+            text: "Build really cool todo app",
+            isCompleted: false
+        }
+    ]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, { text }];
+        setTodos(newTodos);
     };
 
-    addItem = e => {
-        const listArr = this.state.list;
-
-        listArr.push(e);
-
-        this.setState({list: listArr, userInput: ""});
+    const completeTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = true;
+        setTodos(newTodos);
     };
 
-    render() {
-        return (
-            <>
-                <Upper value={this.state.userInput} onChange={e => this.handleChange(e.target.value)} onClick={() => this.addItem(this.state.userInput)}/>
-                {/*<Lower />*/}
-                <ul>
-                    {this.state.list.map(item => <li>{item}</li>)}
-                </ul>
-            </>
-        )
-    }
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="app">
+            <div className="todo-list">
+                {todos.map((todo, index) => (
+                    <Todo
+                        key={index}
+                        index={index}
+                        todo={todo}
+                        completeTodo={completeTodo}
+                        removeTodo={removeTodo}
+                    />
+                ))}
+                <TodoForm addTodo={addTodo} />
+            </div>
+        </div>
+    );
 }
 
 export default Root;
